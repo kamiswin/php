ba0ea7fbb201bf30842505908aaec1c6d665e3b1
kamiswin <kamiswin@users.noreply.github.com>
2016-03-30 16:28:32 -0500

    1
    1

---
+++ empManage.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
+
+<hr/>
+<?php
+	require_once 'common.php';
+	checkUserValidate();
+	//先验证
+	echo "欢迎 ".@$_GET['name']." 登录成功!";
+	echo "<br/><a href='login.php'>返回重新登录</a>";
+	
+	//显示上次登录时间
+	getLastTime();
+	
+	
+	
+	
+?>
+<h1>主界面</h1>
+<a href='empList.php'>管理用户</a><br/>
+<a href='addEmp.php'>添加用户</a><br/>
+<a href='#'>查询用户</a><br/>
+<a href='#'>退出系统</a><br/>
+<hr/>
+
+</html>
